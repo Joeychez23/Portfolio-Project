5664a14dd4a3a308b3c3ff2a27e6fb7c5f33acc3
Joseph <joeychez23@outlook.com>
2026-09-01 06:53:06 -0700

    pain*

--- client/src/App.js
+++ client/src/App.js
@@ -270,7 +270,7 @@ export default function App() {
                 <div>
                   <h4>Education</h4>
                   <p>Full-Stack Development Certification, University of Washington</p>
-                  <o>Computer Programming, Cloud Architecture, Self-Taught</o>
+                  <p>Computer Programming, Cloud Architecture, Self-Taught</p>
                 </div>
               </div>
 
